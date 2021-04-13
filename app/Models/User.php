f90f83c45bf29a781cb2a98599bf985a344dec65
stoletoff <afanasbev1@gmail.com>
2021-04-13 20:45:13 +0300

    feat: add Candy

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function candies()
+    {
+        return $this->hasMany(Candy::class, 'user_id', 'id');
+    }
 }
